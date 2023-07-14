7c07f343f260a15145c91663ac79fdfd3cb09e88
Jobbie <jobvriet@gmail.com>
2023-07-14 15:20:15 +0200

    Sync

--- misc/test_sx42.R
+++ misc/test_sx42.R
@@ -12,32 +12,42 @@ sx42 <- scir::import_samples_monocle(
 )
 
 # Run default monocle3 workflow. ----
-sx42 <- sx42 %>%
+
+# Add additional metadata.
+meta_samples <- readr::read_csv("sx42_metadata.csv")
+sx42$group <- merge(SummarizedExperiment::colData(sx42), meta_samples)$Group
+
+sx42_nonorm <- sx42 %>%
+    monocle3::preprocess_cds(method = "PCA") %>%
+    monocle3::reduce_dimension(., reduction_method = "UMAP", cores = 20) %>%
+    monocle3::cluster_cells(., cluster_method = "leiden")
+
+sx42_normalized <- sx42 %>%
     monocle3::preprocess_cds(method = "PCA") %>%
     monocle3::align_cds(alignment_group = "sample", preprocess_method = "PCA") %>%
     monocle3::reduce_dimension(., reduction_method = "UMAP", cores = 20) %>%
     monocle3::cluster_cells(., cluster_method = "leiden")
 
-# Add additional metadata.
-meta_samples <- readr::read_csv("misc/sx42_metadata.csv")
-sx42$group <- merge(SummarizedExperiment::colData(sx42), meta_samples)$Group
 
 ## Save object. ----
-monocle3::save_monocle_objects(cds=sx42, directory_path='/omics/groups/OE0538/internal/projects/sexomics/runJob/sx42b/rdata/sx42_cds')
+monocle3::save_monocle_objects(cds=sx42_nonorm, directory_path='/omics/groups/OE0538/internal/projects/sexomics/runJob/sx42b/rdata/sx42_nonorm')
+monocle3::save_monocle_objects(cds=sx42_normalized, directory_path='/omics/groups/OE0538/internal/projects/sexomics/runJob/sx42b/rdata/sx42_normalized')
+
+## Load object. ----
+
+sx42_nonorm <- monocle3::load_monocle_objects('/omics/groups/OE0538/internal/projects/sexomics/runJob/sx42b/rdata/sx42_nonorm')
 
 # Plot UMAP ----
 plot_umap <- monocle3::plot_cells(
-    cds = sx42,
+    cds = sx42_nonorm,
     reduction_method = "UMAP",
     label_cell_groups = FALSE,
-    color_cells_by = "group",
+    color_cells_by = "sample",
     cell_size = .2,
     rasterize = TRUE,
     cell_stroke = .2,
     show_trajectory_graph = FALSE,
 ) +
-    ggplot2::labs(x = "UMAP - Dim. 1", y = "UMAP - Dim. 2") +
-    ggplot2::scale_color_manual(values = c('grey50', '#E5193A', '#0AB066', '#526AB0', '#FE9639')) +
-    scir::theme_ggplot()
+    ggplot2::labs(x = "UMAP - Dim. 1", y = "UMAP - Dim. 2")
 
-plot_umap + ggplot2::facet_grid(~group)
+ggplot2::ggsave(filename = '~/test/umap_sx42.tiff', plot = plot_umap + ggplot2::facet_grid(~group), units="px", width = 1800, height = 600, device='tiff', dpi=100)
