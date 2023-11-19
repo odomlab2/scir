eca4d4fd921d17198db86fb880ee35f75befccad
Jobbie <jobvriet@gmail.com>
2023-11-19 15:23:43 +0100

    Fixed object log UMI

--- R/function_import_samples_monocle.R
+++ R/function_import_samples_monocle.R
@@ -133,7 +133,7 @@ import_samples_monocle <- function(folder, samples, gtf, metadata = NULL) {
     monocle3::fData(cds_combined) <- S4Vectors::cbind(data.frame(id = base::gsub("\\..*", "", S4Vectors::rownames(cds_combined))), monocle3::fData(cds_combined))
 
     # Add log10 umi.
-    cds_combined$log.n.umi <- log10(cds$n.umi)
+    cds_combined$log.n.umi <- log10(cds_combined$n.umi)
 
     # Return combined samples.
     return(cds_combined)
